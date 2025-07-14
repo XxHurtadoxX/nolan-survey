f35a49c5207997e12039108534d45cb034c72a10
XxHurtadoxX <155464846+XxHurtadoxX@users.noreply.github.com>
2025-07-14 05:43:37 +0000

    deploy v6

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/nolan-survey/', // muy importante para GitHub Pages
+  base: '', // muy importante para GitHub Pages
 })
